77ae6f92da5582ff4893227287886d0fd0482901
Felix Ndiaye Harris <felixharrisndiaye@gmail.com>
2024-10-29 16:46:47 +0100

    Configuration of clang-format and clangd

--- src/capture.h
+++ src/capture.h
@@ -1,13 +1,17 @@
-#ifndef CAPTURE_H
-#define CAPTURE_H
+// Copyright 2024 Felix Harris Ndiaye
+
+#ifndef SRC_CAPTURE_H_
+#define SRC_CAPTURE_H_
 
 #include <pcap.h>
 
 struct pcap_handler_args {
-   int verbosity;
+  int verbosity;
 };
 
-void capture_loop(char *programe_name, char *interface, char* file, char *filter, int verbosity);
-void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+void capture_loop(char *programe_name, char *interface, char *file,
+                  char *filter, int verbosity);
+void got_packet(u_char *args, const struct pcap_pkthdr *header,
+                const u_char *packet);
 
-#endif
+#endif  // SRC_CAPTURE_H_
--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,9 @@
-#ifndef UTILS_H
-#define UTILS_H
+// Copyright 2024 Felix Harris Ndiaye
+
+#ifndef SRC_UTILS_H_
+#define SRC_UTILS_H_
+
+#include <pcap.h>
 
 void print_devices();
 void print_frame();
@@ -8,4 +12,4 @@ void ether_type_to_name();
 void print_live_capture_summary();
 void print_timestamp(const struct pcap_pkthdr *header);
 
-#endif
+#endif  // SRC_UTILS_H_
